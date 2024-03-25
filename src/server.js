09641ee8102fc93bb0bbb7fcc9f4cbf8e98052bf
LefterDev <leftersotiri@icloud.com>
2024-03-25 16:14:03 +0200

    CORS implementation

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routers/exporter");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("./utilities/connect-db");
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  return res.send("Hello world");
 });
 
 app.listen(
