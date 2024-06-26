e482dcae0ef9fa8c67ddb3790f5ab8873427488d
Lefter Sotiri <leftersotiri@proton.me>
2024-06-26 19:09:01 +0300

    Added boat Router to the endpoints

--- src/routers/exporter.js
+++ src/routers/exporter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const getBoatsRouter = require("./api/getBoats");
+const getBoatRouter = require("./api/getBoat")
 const postBoatApi = require("./api/postBoat");
 const apiMiddleWare = require("../middleware/apiMiddleware");
 
@@ -7,5 +8,5 @@ const apiRouter = Router();
 apiRouter.use(apiMiddleWare);
 apiRouter.use("/get-boats", getBoatsRouter);
 apiRouter.use("/post-boat", postBoatApi);
-
+apiRouter.use("/get-boat", getBoatRouter);
 module.exports = apiRouter;
