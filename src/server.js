21fcf5356cdc12765dac64ed672f5723894aa7e8
Lefter <86067980+LefterDev@users.noreply.github.com>
2024-07-06 10:08:59 +0300

    smal fix

--- src/server.js
+++ src/server.js
@@ -4,11 +4,10 @@ const app = express();
 const apiRouter = require("./routers/exporter");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const ServerlessHttp = require("serverless-http");
 require("./utilities/connect-db");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", apiRouter);
 
-module.exports = ServerlessHttp(app);
+app.listen(8080)
