ae29bfaea8b32f68dd9e1f3b85ed7f62547fc42e
LefterDev <leftersotiri@icloud.com>
2024-07-10 16:22:03 +0300

    Added Authentication Middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,8 @@
+export const authenticationMiddleWare = (req, res, next) => {
+  const key = process.env.SECRET_KEY;
+  const header = req.headers["authorization"];
+
+  if (!header || header !== key)
+    return res.status(403).send({ error: "Access Forbidden" });
+  else next();
+};
